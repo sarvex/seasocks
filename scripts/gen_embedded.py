f789288c63f47214a1980025520645b025ce05ff
Sourcery AI <>
2023-05-12 06:30:24 +0000

    'Refactored by Sourcery'

--- scripts/gen_embedded.py
+++ scripts/gen_embedded.py
@@ -29,10 +29,7 @@ const EmbeddedContent* findEmbeddedContent(const std::string& name) {
 MAX_SLICE = 16
 
 def as_byte(data):
-    if sys.version_info < (3,):
-        return ord(data)
-    else:
-        return data
+    return ord(data) if sys.version_info < (3,) else data
 
 
 def parse_arguments():
@@ -43,20 +40,25 @@ def parse_arguments():
 
 
 def create_file_byte(name, file_bytes):
-    output = []
-    output.append('    const char %s[%d] = {\n' % (name, len(file_bytes) + 1))
-
-    for start in range(0, len(file_bytes), MAX_SLICE):
-        output.append("        " + "".join("'\\x{:02x}',".format(as_byte(x)) for x in file_bytes[start:start+MAX_SLICE]) + "\n")
-    output.append('        0x00,\n')
-    output.append('    };\n')
+    output = ['    const char %s[%d] = {\n' % (name, len(file_bytes) + 1)]
+    output.extend(
+        "        "
+        + "".join(
+            "'\\x{:02x}',".format(as_byte(x))
+            for x in file_bytes[start : start + MAX_SLICE]
+        )
+        + "\n"
+        for start in range(0, len(file_bytes), MAX_SLICE)
+    )
+    output.extend(('        0x00,\n', '    };\n'))
     return ''.join(output)
 
 
 def create_file_info(file_list):
-    output = []
-    for name, base, length in file_list:
-        output.append('        {"/%s", { %s, %d }},\n' % (base, name, length))
+    output = [
+        '        {"/%s", { %s, %d }},\n' % (base, name, length)
+        for name, base, length in file_list
+    ]
     return ''.join(output)
 
 
@@ -64,14 +66,12 @@ def main():
     args = parse_arguments()
 
     files = []
-    index = 1
     file_byte_entries = []
 
-    for file_name in args.input_file:
+    for index, file_name in enumerate(args.input_file, start=1):
         with open(file_name, 'rb') as f:
             file_bytes = f.read()
         name = "fileData%d" % index
-        index += 1
         files.append((name, os.path.basename(file_name), len(file_bytes)))
         file_byte_entries.append(create_file_byte(name, file_bytes))
 
